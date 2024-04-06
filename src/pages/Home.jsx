d3374803dfd972bf3ee5ef7b570b3ce1543837c2
Aniket <aniketchaudharyjavaprog@gmail.com>
2024-04-06 19:04:04 +0530

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import { Space } from "../models/Space";
+import { Island } from "../models/Island";
+import { Sky } from "../models/Sky";
+import { Color } from "three";
+import { colors } from "@react-spring/shared";
+{
+  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center ">
+Popup
+</div> */
+}
+const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+  const adjustSpaceForScreenSize = () => {
+    let screenScale;
+    let screenPostion = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPostion];
+  };
+  const [spaceScale, spacePosition, rotation] = adjustSpaceForScreenSize();
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className={`w-full h-screen bg-black ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight
+            position={[10, 10, 1]}
+            intensity={1}
+            color={colors.red}
+          />
+          <ambientLight intensity={1} color={0x0000ff} />
+          <pointLight intensity={1} color={0x0000ff} />
+          <spotLight intensity={1} color={0x0000ff} />
+          <Space
+            position={spacePosition}
+            scale={spaceScale}
+            rotation={rotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
